5e745e799b995254c18f5946f67b097b5cd194e6
benedikt <benedikt.reich05@gmail.com>
2024-08-13 22:48:15 +0200

    Cleaned Up the Copyright class

--- src/frontend/src/components/Copyright/Copyright.tsx
+++ src/frontend/src/components/Copyright/Copyright.tsx
@@ -2,19 +2,11 @@ import React from 'react';
 
 const Copyright = () => {
     return (
-        <div className="bg-base dark:bg-darkmode-base text-text-variant dark:text-darkmode-text-variant border-t border-solid border-base-variant dark:border-darkmode-base-variant text-sm text-center m-0 p-4">
+        <div
+            className="bg-base dark:bg-darkmode-base text-text-variant dark:text-darkmode-text-variant border-t border-solid border-base-variant dark:border-darkmode-base-variant text-sm text-center m-0 p-4">
             <p>© Benedikt Reich. All right reserved.</p>
         </div>
     );
-    /*.copyright {
-    background-color: var(--base-color);
-    color: var(--text-color-variant);
-    border-top: 1px solid var(--base-color-variant);
-    font-size: 0.8rem;
-    text-align: center;
-    margin: 0;
-    padding: 1rem;
-}*/
 }
 
 export default Copyright;

b58189d7dc5d58d407ff864795d87269a490a414
benedikt <benedikt.reich05@gmail.com>
2024-09-25 12:29:37 +0200

    Changed the logging for Blocked Origins from console.log to own logging class. Also adjusted the allowedOrigins

--- src/backend/src/middleware/corsHandler.ts
+++ src/backend/src/middleware/corsHandler.ts
@@ -1,13 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function corsHandler(req: Request, res: Response, next: NextFunction) {
-    const allowedOrigins = ['http://localhost:1337', 'http://localhost', 'http://localhost:8080', 'http://daily-dinner.com'];
+    const allowedOrigins = ['http://192.168.178.169', 'https://192.168.178.169', 'http://daily-dinner.com', 'https://daily-dinner.com'];
     const origin = req.headers.origin as string;
 
     if (allowedOrigins.includes(origin) || !origin) {
         res.header('Access-Control-Allow-Origin', origin || '*');
     } else {
-        console.log(`Blocked Origin: ${origin}`);
+        logging.log(`Blocked Origin: ${origin}`);
     }
 
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');

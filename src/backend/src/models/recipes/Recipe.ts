3394442259d7c7565850b02565e9664982990bf4
benedikt <benedikt.reich05@gmail.com>
2024-12-10 16:34:22 +0100

    Removed the backend from this project

--- src/backend/src/models/recipes/Recipe.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-import RecipeSchema from './RecipeSchema';
-import { IRecipe } from './IRecipe';
-
-const Recipe = mongoose.model<IRecipe>('Recipe', RecipeSchema);
-
-export default Recipe;
